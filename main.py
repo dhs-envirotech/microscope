e3ac25b0059ad4f6f6d60fda08fd78201d150b35
nakulsoneji <nakulsoneji@gmail.com>
2023-10-17 17:00:41 -0700

    added contours to images, may remove later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-# import the opencv library
+# import the opencv2.library
 import cv2
 
 
@@ -7,11 +7,13 @@ import cv2
 vid = cv2.VideoCapture(1)
 
 while(True):
-
     # Capture the video frame
     # by frame
     ret, frame = vid.read()
-
+    imgray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(frame, contours, -1, (0,255,0), 1)
     # Display the resulting frame
     cv2.imshow('frame', frame)
 
